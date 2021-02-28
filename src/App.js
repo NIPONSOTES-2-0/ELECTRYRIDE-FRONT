2905e7b272b567747093e3a280c74e289ab48e14
Eduard Jimenez <eduard.jimenez@mail.escuelaing.edu.co>
2021-02-28 17:33:14 -0500

    IR-002: Navigation

--- src/App.js
+++ src/App.js
@@ -1,37 +1,38 @@
-
+import React, { Component } from "react";
 import "./App.css";
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import UserProfile from "./components/profile/UserProfile";
 import Navbar from "./components/navbar/Navbar";
 
-function App() {
-  return (
-    <div className="App">    
-      <Router>
-        <Navbar />
-        <Switch>          
-        <Route exact path="/" >
-            <h1>Index</h1>
-          </Route>      
-          <Route exact path="/perfil" >
-            <UserProfile />
-          </Route>          
-          <Route exact path="/map" >
-            <h1>Mapa</h1>
-          </Route> 
-          <Route exact path="/viaje" >
-            <h1>Inicia tu viaje</h1>
-          </Route>   
-          <Route exact path="/pagos" >
-            <h1>Agregar medio de pago</h1>
-          </Route>   
-          <Route exact path="/ayuda" >
-            <h1>Contacta con nosotros</h1>
-          </Route>   
-        </Switch>
-      </Router>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const UserProfileView = () => <UserProfile />;
+    return (
+      <div className="App">
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path="/">
+              <h1>Inicio</h1>
+            </Route>
+            <Route path="/perfil" component={UserProfileView} />
+            <Route exact path="/map">
+              <h1>Mapa</h1>
+            </Route>
+            <Route exact path="/viaje">
+              <h1>Inicia tu viaje</h1>
+            </Route>
+            <Route exact path="/pagos">
+              <h1>Escoge tu metodo de pago</h1>
+            </Route>
+            <Route exact path="/ayuda">
+              <h1>Contacta con nosotros</h1>
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
