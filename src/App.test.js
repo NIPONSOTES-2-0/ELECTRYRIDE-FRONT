db76311ea4da6dba17cfbfa59d1caa06c936fe46
Eduard Jimenez <eduard.jimenez@mail.escuelaing.edu.co>
2021-03-01 22:43:49 -0500

    IR-004: Markers and Popups

--- src/App.test.js
+++ src/App.test.js
@@ -1,15 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import ViewMap from './components/map/ViewMap';
-import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import ViewMap from "./components/map/ViewMap";
+import CustomMarker from "./components/map/CustomMarker";
+import CustomPopup from "./components/map/CustomPopup";
+import venues from "./components/map/places.json";
+import Markers from "./components/map/Markers";
+import { render, screen, fireEvent, waitFor } from "@testing-library/react";
+import { mount } from "enzyme";
+import { configure } from "enzyme";
+//import Adapter from "enzyme-adapter-react-16";
+//configure({ adapter: new Adapter() });
 
-it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-    ReactDOM.unmountComponentAtNode(div);
+
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
 
-test('Should return map', () => {
-    render(<ViewMap />)
+
+test("Should Put the Marks", () => {    
+    const places = venues.venues;    
+    render(<ViewMap />);
+    const handleChange = jest.fn();
+    const x = (popup) => {console.log(popup)};
+    const markerrs = render(<Markers  places={places} openPopup={x}/>);             //places={places}
+    console.log("markerrs: ",markerrs);
   });
+
+  
+test("Should return map", () => {
+  render(<ViewMap />);
+});
+
+//const buttons = screen.getAllByRole("button");
+
+it("Should Have a Mark", () => {
+  const  marker =  {
+    description: 'Centro comercial Santa fe',
+    name: 'C.C Santa Fe',
+    latitude: 4.752188061011153,
+    longitude: -74.04425755035315,
+    bikes_aviables: 0
+  };
+
+  const onClick = jest.fn();      
+  const customMarker = render(<CustomMarker marker={marker} openPopup={onClick}/>)
+  console.log("customMarker: ",customMarker);  
+  const mark = screen.getAllByRole("div");  
+  fireEvent.click(mark);
+
+});
+
+
+test('Should show a Popup', () => {
+    const  marker =  {
+        description: 'Centro comercial Santa fe',
+        name: 'C.C Santa Fe',
+        latitude: 4.752188061011153,
+        longitude: -74.04425755035315,
+        bikes_aviables: 0
+      };
+    render(<CustomPopup index={1} marker={marker} />);
+});
+
+/*
+const searchProccess = (searchValue) => {
+    const buttons = screen.getAllByRole("button");
+    console.log("Buttons: ",buttons);
+    console.log("ESTE ES EL BOTON: ",buttons[0]);
+    const searchButton = buttons[1];
+    console.log("Boton de busqueda: ",searchButton);
+    fireEvent.click(searchButton);
+    //const secondSearchButton = screen.getByRole("button");
+    //const searchInput = screen.getByRole("textbox");
+    //fireEvent.change(searchInput, { target: { value: searchValue } })
+    //fireEvent.click(secondSearchButton);
+  };
+  */
+test("Should Search A Place", () => {
+  render(<ViewMap />);
+  //searchProccess("Escuela Colombiana");
+  //expect(screen.getByText(/Escuela Colombiana de Ingeniería Julio Garavito/i)).toBeInTheDocument();
+});
--- src/components/map/ViewMap.js
+++ src/components/map/ViewMap.js
@@ -1,57 +1,86 @@
-import React, { Component } from 'react';
-import ReactMapGL from 'react-map-gl';
-import Geocoder from 'react-mapbox-gl-geocoder';
-import { Container, Col, Row } from 'reactstrap';
+import React, { Component } from "react";
+import ReactMapGL from "react-map-gl";
+import Geocoder from "react-mapbox-gl-geocoder";
+import { Container, Col, Row, Button } from "reactstrap";
+import 'mapbox-gl/dist/mapbox-gl.css';
+//import '@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css'
+
+
+import { Marker } from "react-map-gl";
+import "./styles/Marker.css";
+import Markers from "./Markers";
+import venues from "./places.json";
+
 
 const mapStyle = {
-    width: '100%',
-    height: 600,
-    zIndex: 1
-}
+  width: "100%",
+  height: 600,
+  zIndex: 1,
+};
 
-const mapboxApiKey = 'pk.eyJ1IjoiZWRraWxsYWgiLCJhIjoiY2tsM3k0ZzBnMGF3MjJvanUyYjRmdDQzZSJ9.4UIPAYjND8HNAZEsCa8e4A'
+const mapboxApiKey =
+  "pk.eyJ1IjoiZWRraWxsYWgiLCJhIjoiY2tsM3k0ZzBnMGF3MjJvanUyYjRmdDQzZSJ9.4UIPAYjND8HNAZEsCa8e4A";
 
 const params = {
-    country: "co"
-}
+  country: "co",
+};
 
 class MapView extends Component {
-
-  constructor(props) {    
+  constructor(props) {
     super(props);
     this.state = {
       viewport: {
         latitude: 4.7653385,
         longitude: -74.0507561,
-        zoom: 15
-      }
+        zoom: 15,
+      },
+      tempMarker: null,
+      markers: [],
+      selectedIndex: null,
+      activePopup: null
     };
-
   }
+  
+  openPopup = (popup) => {        
+    this.setState({ activePopup: popup });
+  };
 
-  onSelected = (viewport, item) => {      
-      this.setState({
-        viewport
-      })
-  }
+  onSelected = (viewport, item) => {
+    this.setState({
+      viewport,
+      tempMarker: {
+        name: item.place_name,
+        longitude: item.center[0],
+        latitude: item.center[1],
+      },
+    });
+  };
+
 
   render() {
-    const view = this.state.viewport;     
-    return(
-      <Container fluid={true}> 
+    const view = this.state.viewport;    
+    return (
+      <Container fluid={true}>
         <Row>
-          <Col><h2>Mapa</h2></Col>
+          <Col>
+            <h2>Mapa</h2>
+          </Col>
         </Row>
         <Row className="py-4">
           <Col xs={2}>
-            <Geocoder 
-                mapboxApiAccessToken={mapboxApiKey}
-                onSelected={this.onSelected}
-                viewport={view}
-                hideOnSelect={true}
-                value=""
-                queryParams={params}
+            <Geocoder
+              mapboxApiAccessToken={mapboxApiKey}
+              onSelected={this.onSelected}
+              viewport={view}
+              hideOnSelect={true}
+              value=""
+              queryParams={params}
             />
+          </Col>
+          <Col>
+            <Button color="primary">
+              Add
+            </Button>
           </Col>
         </Row>
         <Row>
@@ -61,13 +90,25 @@ class MapView extends Component {
               mapStyle="mapbox://styles/mapbox/streets-v11"
               {...view}
               {...mapStyle}
-              onViewportChange={(viewport) => this.setState({viewport})}
+              onViewportChange={(viewport) => this.setState({ viewport })}
             >
+              {this.state.tempMarker && (
+                <Marker
+                  longitude={this.state.tempMarker.longitude}
+                  latitude={this.state.tempMarker.latitude}
+                >
+                  <div className="marker temporary-marker">
+                    <span></span>
+                  </div>
+                </Marker>
+              )}              
+              <Markers places={venues.venues} openPopup={this.openPopup}/>
+             {this.state.activePopup !== null && (this.state.activePopup)}
             </ReactMapGL>
           </Col>
         </Row>
       </Container>
-   );
+    );
   }
 }
 
