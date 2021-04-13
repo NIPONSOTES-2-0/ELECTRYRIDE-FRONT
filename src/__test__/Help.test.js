7eb641a58a8073a5ec94f0aef5104352c2815cf6
Miguel Sanchez <miguel.sanchez-g@mail.escuelaing.edu.co>
2021-04-13 16:56:12 -0500

    arreglando pruebas

--- src/__test__/Help.test.js
+++ src/__test__/Help.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import FormularioAyuda from '../components/ayuda/FormularioAyuda';
+import {FormularioAyuda} from '../components/ayuda/FormularioAyuda';
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 
 
