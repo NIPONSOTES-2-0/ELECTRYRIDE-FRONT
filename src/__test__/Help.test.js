d6691a94368d81b7b907750d11b72549116c030d
Miguel Sanchez <miguel.sanchez-g@mail.escuelaing.edu.co>
2021-04-16 13:59:54 -0500

    arreglando errores

--- src/__test__/Help.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {FormularioAyuda} from '../components/ayuda/FormularioAyuda';
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-
-
-
-test("Should render help", () => {
-  render(<FormularioAyuda />);
-});
--- src/__test__/Support.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {FormularioApoyo} from '../components/apoyo/FormularioApoyo';
-import { render, screen, fireEvent, waitFor } from "@testing-library/react";
-
-
-
-test("Should render support", () => {
-  render(<FormularioApoyo />);
-});
